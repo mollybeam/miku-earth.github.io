6a3eca34ff794b743157734401102f43f1029a2f
Mia yun Ruse <mia@yunru.se>
2024-08-30 15:21:31 +0100

    add miqueue.py
    i've got one or two requests! this allows me to see if a region is in the queue or not without combing through the whole queue lol

---
+++ miqueue.py
@@ -0,0 +1,24 @@
+"""
+Display the queue and its tags.
+"""
+
+from pytumblr2 import Post
+
+from helpers import client, process_tags
+
+N = 1
+
+for post in client.get_posts("miku-earth", queue=True):
+    N += 1
+    page = N // 20
+
+    # print(post)
+    post: Post
+
+    miku = process_tags(post['tags'])
+
+    url = post['id_string']
+    source = miku['source']
+    loc = ', '.join(miku['loc'])
+    
+    print(f'{page:2} {miku['continent']:10} {loc}')
