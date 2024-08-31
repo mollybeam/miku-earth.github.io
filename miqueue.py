010feec424d2869fb3188a8cfb936d81c5515d04
Mia yun Ruse <mia@yunru.se>
2024-08-31 22:19:53 +0100

    15 more mikus!

--- miqueue.py
+++ miqueue.py
@@ -5,7 +5,12 @@ Display the queue and its tags.
 
 from pytumblr2 import Post
 
-from helpers import client, process_tags
+from helpers import client, load_mikus, process_tags
+
+for miku in load_mikus():
+    artist = miku['artist']
+    loc = ', '.join(miku['loc']) if 'loc' in miku else miku['name']
+    print(f'   {artist:20} {loc}')
 
 N = 0
 for post in client.get_posts("miku-earth", queue=True):
@@ -21,5 +26,6 @@ for post in client.get_posts("miku-earth", queue=True):
     url = post['id_string']
     source = miku['source']
     loc = ', '.join(miku['loc']) if 'loc' in miku else miku['name']
+    artist = miku['artist']
 
-    print(f'{page:2} {miku['artist']:20} {loc}')
+    print(f'{page:2} {artist:20} {loc}')
