6291c603cfcccdd8283906894a579c792de26cd3
Mia yun Ruse <mia@yunru.se>
2024-08-31 18:34:20 +0100

    improve queue system

--- miqueue.py
+++ miqueue.py
@@ -7,19 +7,19 @@ from pytumblr2 import Post
 
 from helpers import client, process_tags
 
-N = 1
-
+N = 0
 for post in client.get_posts("miku-earth", queue=True):
     N += 1
-    page = N // 20
+    page = N // 20 + 1
 
     # print(post)
     post: Post
-
+    
     miku = process_tags(post['tags'])
+    miku.setdefault('artist', post['trail'][-1]['blog']['name'])
 
     url = post['id_string']
     source = miku['source']
     loc = ', '.join(miku['loc']) if 'loc' in miku else miku['name']
 
-    print(f'{page:2} {loc}')
+    print(f'{page:2} {miku['artist']:20} {loc}')
